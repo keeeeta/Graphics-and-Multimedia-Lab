d1a654b8923694d2dcb6d6605f3835a40be61d0d
Keerthana Thiagarajan <72556340+keeeeta@users.noreply.github.com>
2021-08-12 12:36:52 +0530

    Add files via upload

---
+++ ex4/b.cpp
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <iostream>
+#include <GL/glut.h>
+using namespace std;
+
+int pntX1, pntY1, r;
+
+void plot(int x, int y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x + pntX1, y + pntY1);
+	glEnd();
+}
+
+
+void myInit(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(-200, 200, -200, 200);
+}
+
+void vada(int r)
+{
+	int x = 0;
+	int y = r;
+	float decision = 5 / 4 - r;
+	plot(x, y);
+
+	while (y > x)
+	{
+		if (decision < 0)
+		{
+			x++;
+			decision += 2 * x + 1;
+		}
+		else
+		{
+			y--;
+			x++;
+			decision += 2 * (x - y) + 1;
+		}
+		plot(x, y);
+		plot(x, -y);
+		plot(-x, y);
+		plot(-x, -y);
+		plot(y, x);
+		plot(-y, x);
+		plot(y, -x);
+		plot(-y, -x);
+	}
+
+}
+
+void midPointCircleAlgo()
+{
+	glPointSize(18.0);
+	glColor3f(0.9, 0.5, 0.1);
+	vada(r);
+	glPointSize(8.0);
+	glColor3f(1, 0, 1);
+	glPolygonMode(GL_FRONT, GL_FILL);
+	vada(r - 10);
+}
+
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0, 0.0, 0.0);
+	
+	midPointCircleAlgo();
+
+	glFlush();
+}
+
+void main(int argc, char** argv)
+{
+	cout << "Enter the coordinates of the center:\n\n" << endl;
+
+	cout << "X-coordinate   : "; cin >> pntX1;
+	cout << "\nY-coordinate : "; cin >> pntY1;
+	cout << "\nEnter radius : "; cin >> r;
+
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Doughnut");
+	glutDisplayFunc(myDisplay);
+	myInit();
+	glutMainLoop();
+
+}
+
+
+/*#include <stdio.h>
+#include <iostream>
+#include <GL/glut.h>
+using namespace std;
+
+int pntX1, pntY1, r;
+
+void plot(int x, int y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x + pntX1, y + pntY1);
+	glEnd();
+}
+
+
+void myInit(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(-200, 200, -200, 200);
+}
+
+
+void midPointCircleAlgo()
+{
+	int x = 0;
+	int y = r;
+	float decision = 5 / 4 - r;
+	plot(x, y);
+
+	while (y > x)
+	{
+		if (decision < 0)
+		{
+			x++;
+			decision += 2 * x + 1;
+		}
+		else
+		{
+			y--;
+			x++;
+			decision += 2 * (x - y) + 1;
+		}
+		plot(x, y);
+		plot(x, -y);
+		plot(-x, y);
+		plot(-x, -y);
+		plot(y, x);
+		plot(-y, x);
+		plot(y, -x);
+		plot(-y, -x);
+	}
+
+}
+
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0, 0.0, 0.0);
+	glPointSize(5.0);
+	
+	glColor3f(1.0,0.5,0.0);
+	glPolygonMode(GL_FRONT, GL_FILL);
+	midPointCircleAlgo();
+	
+	glFlush();
+}
+
+void main(int argc, char** argv)
+{
+	cout << "Enter the coordinates of the center:\n\n" << endl;
+
+	cout << "X-coordinate   : "; cin >> pntX1;
+	cout << "\nY-coordinate : "; cin >> pntY1;
+	cout << "\nEnter radius : "; cin >> r;
+
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Midpoint circle algorithm");
+	glutDisplayFunc(myDisplay);
+	myInit();
+	glutMainLoop();
+
+}*/
